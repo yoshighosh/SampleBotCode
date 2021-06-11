b8bb88c042fc4ce96effb006804c158b96ede612
yoshighosh <yoshighosh@gmail.com>
2021-06-11 22:45:05 +0000

    Add match scouting command

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ bot.remove_command('help')
 
 @bot.command()
 async def hello(ctx):
-   await ctx.channel.send("Hello!")
+  await ctx.channel.send("Hello!")
 
 @bot.command(name="help")
 async def commands(ctx):
-   await ctx.channel.send(Commands.commandList)
+  await ctx.channel.send(Commands.commandList)
 
 @bot.command()
-async def get(ctx, arg):
-  if arg == "CAD":
-     await ctx.channel.send('Here is the link to the CAD: https://ftc8404-ultimate-goal-robot-360.netlify.app/')
-  if arg == "PALS":
-     await ctx.channel.send('Here is the link to the PALS homepage: https://pals.quixilver8404.org/')
+async def get(ctx, *args):
+  command = ""
+  print(args)
+  for arg in args:
+    command += arg
+  print(command)
+  if command == "CAD":
+    await ctx.channel.send('Here is the link to the CAD: https://ftc8404-ultimate-goal-robot-360.netlify.app/')
+  if command == "PALS":
+    await ctx.channel.send('Here is the link to the PALS homepage: https://pals.quixilver8404.org/')
+  if command == "matchscouting":
+    await ctx.channel.send('Here is the link to the PALS match scouting page: https://pals.quixilver8404.org/match-scouting')
 
 
 keep_alive()
