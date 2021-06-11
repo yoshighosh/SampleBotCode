741044ca3a9aea7ffc664ec725328d68d4fb8d7b
yoshighosh <yoshighosh@gmail.com>
2021-06-11 23:06:53 +0000

    Add get pregame scouting command

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ async def commands(ctx):
 @bot.command()
 async def get(ctx, *args):
   command = ""
-  print(args)
   for arg in args:
     command += arg
-  print(command)
   if command == "CAD":
     await ctx.channel.send('Here is the link to the CAD: https://ftc8404-ultimate-goal-robot-360.netlify.app/')
-  if command == "PALS":
+  elif command == "PALS":
     await ctx.channel.send('Here is the link to the PALS homepage: https://pals.quixilver8404.org/')
-  if command == "matchscouting":
+  elif command == "matchscouting":
     await ctx.channel.send('Here is the link to the PALS match scouting page: https://pals.quixilver8404.org/match-scouting')
+  elif command == "pregamescouting":
+    await ctx.channel.send('Here is the link to the PALS pregame scouting page: https://pals.quixilver8404.org/pre-game-scouting')
 
 
 keep_alive()
